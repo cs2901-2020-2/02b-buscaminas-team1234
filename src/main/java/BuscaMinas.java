3b33d65fb9d1f44f875932ceeb453aa08fc4e410
eduardosalas1 <eduardo.salas@utec.edu.pe>
2020-09-11 16:15:36 -0500

    otra tipo de buscaminas

--- src/main/java/BuscaMinas.java
+++ src/main/java/BuscaMinas.java
@@ -1,63 +1,89 @@
-import java.util.Random;
-import java.util.Scanner;
+import java.util.*;
 import java.util.logging.*;
 
+public class BuscaMinas{
 
-public class BuscaMinas {
-	private static final int MINA = -1;
-	private static final int CASILLA = 0;
-	private static final int CASILLA_ABIERTA = 2;
+    private static final char Mina = '*';
+    private static final char Casilla = '-';    
+	private static  int Bufferx = 5;
+    private static  int Buffery = 5;
+    
 
-	static final Logger logger = Logger.getLogger(BuscaMinas.class.getName());
 
-	private static boolean check_if_win(int[][] board) {
-		return false;
-    }
+    static final Logger logger = Logger.getLogger(BuscaMinas.class.getName());
 
-	public static int[][] generate_board(int ancho, int alto) {
-	    int [][]board = new int[ancho][alto];
-	    Random rand = new Random();
-
-	    for (int iterador_ancho = 0; iterador_ancho < ancho; iterador_ancho++) {
-	    	for (int iterador_alto = 0; iterador_alto < alto; iterador_alto++) {
-				if (rand.nextInt(2) == 0)
-					board[iterador_ancho][iterador_alto] = CASILLA;
-				else
-					board[iterador_ancho][iterador_alto] = MINA;
-			}
-	    }
-	    return board;
-    }
-
-    public static boolean select_space(int ancho, int alto, int [][] board) {
-    	if (board[ancho][alto] == CASILLA) {
-			board[ancho][alto] = CASILLA_ABIERTA;
-			return false;
-		}
-		else if(board[ancho][alto] == CASILLA_ABIERTA){
-			logger.info("Casilla llena!");
-			return false;
-		}
-		else{
-			logger.info("Perdiste!");
-			return true;
-
-		}
-    }
-
-    public boolean check_if_win (int [][]board, int ancho, int alto) {
+    public static boolean check_if_win (char [][] board) {
     	boolean win = true;
-		for (int iterador_ancho = 0; iterador_ancho < ancho; iterador_ancho++) {
-			for (int iterador_alto = 0; iterador_alto < alto; iterador_alto++) {
-				if (board[iterador_ancho][iterador_alto] == CASILLA)
+		for (int iterador_ancho = 0; iterador_ancho < Bufferx; iterador_ancho++) {
+			for (int iterador_alto = 0; iterador_alto < Buffery; iterador_alto++) {
+				if (board[iterador_ancho][iterador_alto] == Casilla)
 					win = false;
 			}
 		}
 		return win;
     }
 
-	public static void main (String [] args) {
-		Scanner input = new Scanner (System.in);
+    public static char[][] generate_board(int ancho, int largo){
+
+		Random rand = new Random();
+		
+		char [][]board = new char[ancho][largo];
+
+        Bufferx = ancho-1;
+        Buffery = largo-1;
+
+        for(int i = 0 ; i < ancho ; i++){
+            for (int j = 0 ; j < largo ; j++){
+                
+                int randint = rand.nextInt(2);
+
+                if(randint == 0) board[i][j] = Casilla;
+
+                else board[i][j] = Mina;                
+
+            }
+		}
+		
+		return board;
+    }
+
+    public static int entorno(int posx, int posy,char[][] board){
+
+        int contador = 0;
+
+        for(int i = 0 ; i <= Bufferx ; i++){
+            for(int j = 0 ; j <= Buffery ; j++){
+                if(i - 1 == posx || i +1 == posx || i == posx){
+                    if(j-1 == posy || j+1 == posy || j == posy){
+                        if(board[i][j] == Mina) contador++;
+                        continue;
+
+                    }
+                }
+            }
+        }
+        return contador;
+
+    }
+
+    public static boolean select_space(int posx, int posy,char[][] board){
+
+        if(board[posx][posy] == Casilla){
+            board[posx][posy] = (char)entorno(posx, posy,board);
+            return false;          
+
+        }
+        else if(board[posx][posy] != Mina || board[posx][posy] != Casilla){
+            logger.info("Esta casilla ya la tomaste!");
+            return false;
+        }
+        else return true;
+
+    }
+
+    public static void main(String args[]){
+
+        Scanner input = new Scanner (System.in);
 		int ancho, alto;
 
 		while(true){
@@ -82,7 +108,7 @@ public class BuscaMinas {
 			}
 		}
 
-		int [][]board = generate_board (ancho, alto);
+		char[][] board = generate_board (ancho, alto);
 
 		boolean over = false;
 
@@ -101,17 +127,15 @@ public class BuscaMinas {
 				logger.warning ("¡Ancho fuera de rango! Maximo alto de: "+(alto-1));
 				continue;
 			}
-			if (select_space (input_ancho, input_alto, board)) {
+			if (select_space (input_ancho, input_alto,board)) {
 				logger.info ("Encontraste una mina! Perdiste :(");
 				return;
 			}
-			if (check_if_win (board)) {
+			if (check_if_win(board)) {
 				logger.info ("Ganaste :)");
 				return;
 			}
 		}
 
-
-	}
-
+    }   
 }
